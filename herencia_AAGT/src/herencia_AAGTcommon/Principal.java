c10335bd2d6872837ee786a609b4c8b73a457b45
AntonioTorresdey <to_tony_torresdey@hotmail.com>
2024-01-31 19:31:39 -0600

    Herencia1clase

---
+++ herencia_AAGT/src/herencia_AAGTcommon/Principal.java
@@ -0,0 +1,9 @@
+package herencia_AAGTcommon;
+
+public class Principal {
+
+	public static void main(String[]args) {
+		
+		
+	}
+}
---
+++ herencia_AAGT/src/herencia_AAGTcommon/Smartwatch.java
@@ -0,0 +1,34 @@
+package herencia_AAGTcommon;
+
+public class Smartwatch extends Reloj{
+	
+	SistemaOperativo so;
+	
+	public Smartwatch() {
+		
+	}
+	
+	
+	public Smartwatch(String tipo, int minutos, int horas, int segundos, String material, SistemaOperativo so) {
+		super();
+		this.so = so;
+	}
+	
+	
+	//Metodo nativo 
+	public void sincronizarHora() {
+		
+		System.out.println("Conectando a internet ....");
+		System.out.println("Sincronizando hora...");
+	}
+	
+	@Override
+	public void mostrarHora() {
+		System.out.println("Esta es la hora .....");
+	}
+	
+	
+	
+
+	
+}
