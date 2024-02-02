fa61ac310fb455ed1fbe91f0e86e0d2a584ab3ad
Al3cxz <124101802+Al3cxz@users.noreply.github.com>
2024-02-01 18:06:07 -0600

    Practica Calculador_AVM

---
+++ Interfaces_AVM/src/InterfazTrabajo/Basicas.java
@@ -0,0 +1,13 @@
+package InterfazTrabajo;
+
+public interface Basicas {
+	
+	double sumar(double a, double b);
+
+	double restar(double a, double b);
+
+	double multiplicar(double a, double b);
+
+	double dividir(double a, double b);
+	
+}
---
+++ Interfaces_AVM/src/InterfazTrabajo/Cientifica.java
@@ -0,0 +1,8 @@
+package InterfazTrabajo;
+
+public interface Cientifica {
+	double potencia(double base, double exponent);
+
+	double raizC(double numero);
+
+}
